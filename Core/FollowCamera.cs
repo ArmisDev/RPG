b05a431c1e33261079678e45f391fe2f1d2e75ed
ArmisDev <garrettburrdev@gmail.com>
2023-08-03 20:37:47 -0500

    Core
    Core Functionality

---
+++ Core/FollowCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] private GameObject target;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            transform.position = target.transform.position;
+        }
+    }
+}
---
+++ Core/Health.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] private float health = 100f;
+        public bool hasDied = false;
+
+        public void TakeDamage(float damage)
+        {
+            health = Mathf.Max(health - damage, 0);
+            if(health == 0)
+            {
+                HandleDeath();
+            }
+        }
+
+        public bool IsDead()
+        {
+            return hasDied;
+        }
+
+        void HandleDeath()
+        {
+            if (hasDied) return;
+
+            hasDied = true;
+            GetComponent<Animator>().SetTrigger("Killed");
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+    }
+}
