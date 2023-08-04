405d80c0f5d9936908cffe6d547f5ead12fa0521
ArmisDev <garrettburrdev@gmail.com>
2023-08-03 20:39:09 -0500

    Control
    Controller Functionality

---
+++ Control/AIController.cs
@@ -0,0 +1,166 @@
+using UnityEngine;
+using System.Collections;
+using RPG.Combat;
+using RPG.Core;
+using RPG.Movement;
+using UnityEngine.AI;
+using System;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] private float suspsionStateTime = 7f;
+        [SerializeField] private float chaseRange = 5f;
+        [SerializeField] private GameObject target;
+        [SerializeField] private PatrolPath patrolPath;
+        [SerializeField] private float partolWaitime;
+        [SerializeField] private float wayPointTolerence = 1f;
+        [SerializeField] [Tooltip("Set false if attaching a patrol path")] private bool isGuarding = true;
+
+        private Fighter fighter;
+        private Health health;
+        private Mover mover;
+        private NavMeshAgent navMeshAgent;
+
+        //Chase Timer
+        private float timeSinceLastSawPlayer = Mathf.Infinity;
+
+        //Private patrol timer
+        public float patrolTimeIncrement = 0f;
+
+        //Ammount position can be off
+        private float positionOffsetTolerence = 0.15f;
+
+        private Vector3 guardPosition;
+        private Vector3 lastPosition;
+        private Quaternion guardRotation;
+        private int currentIndexWaypoint = 0;
+
+        private float currentAISpeed;
+        private float returnSpeed = 1.895f;
+
+        private void Start()
+        {
+            //Grabs fighter class
+            fighter = GetComponent<Fighter>();
+            //Assigns player as target
+            target = GameObject.FindGameObjectWithTag("Player");
+            //Grabs Health
+            health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+
+            guardPosition = transform.position;
+            guardRotation = transform.rotation;
+
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            currentAISpeed = navMeshAgent.speed;
+        }
+
+        private void Update()
+        {
+            if (health.IsDead()) return;
+            //Checks to see if AI should chase
+            if (InAttackRange() && fighter.CanAttack(target))
+            {
+                //Sets AI speed to base speed if it was resetting during guard behavior
+                if(navMeshAgent.speed != currentAISpeed)
+                {
+                    navMeshAgent.speed = currentAISpeed;
+                }
+
+                AttackBehavior();
+                timeSinceLastSawPlayer = 0f;
+            }
+
+            else if(timeSinceLastSawPlayer < suspsionStateTime)
+            {
+                SuspsionBehavior();
+            }
+
+            else
+            {
+                PatrolBehavior();
+            }
+
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void AttackBehavior()
+        {
+            fighter.Attack(target);
+        }
+
+        private void SuspsionBehavior()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void PatrolBehavior()
+        {
+            Vector3 nextPosition = guardPosition;
+
+            if(patrolPath != null)
+            {
+                if(AtWayPoint())
+                {
+                    //Cycle to next waypoint
+                    CycleWaypoint();
+                }
+                nextPosition = GetCurrentWaypoint();
+            }
+
+            mover.StartMoveAction(nextPosition);
+
+            //Rotates player if they have moved back to their position
+            if (isGuarding)
+            {
+                navMeshAgent.speed = returnSpeed;
+
+                if (HasReturnedFromChase() && transform.rotation != guardRotation)
+                {
+                    Debug.Log("Should Rotate");
+                    transform.rotation = guardRotation;
+                }
+            }
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currentIndexWaypoint);
+        }
+
+        private void CycleWaypoint()
+        {
+            currentIndexWaypoint = patrolPath.GetNextIndex(currentIndexWaypoint);
+        }
+
+        private bool AtWayPoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < wayPointTolerence;
+        }
+
+        //Checks to see if player is within attacking range.
+        private bool InAttackRange()
+        {
+            float distanceCheck = Vector3.Distance(transform.position, target.transform.position);
+            return distanceCheck < chaseRange;
+        }
+
+        //Returns true if AI is near their original position
+        //Essentially is equivilent to Mathf.Approximetly
+        private bool HasReturnedFromChase()
+        {
+            float xDifference = Mathf.Abs(transform.position.x - guardPosition.x);
+            return xDifference <= positionOffsetTolerence;
+        }
+
+        //Called by Unity
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(transform.position, chaseRange);
+        }
+    }
+}
---
+++ Control/PlayerController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using RPG.Combat;
+using RPG.Core;
+using System;
+using UnityEngine.AI;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public bool canAttack;
+        private Health health;
+
+        private void Start()
+        {
+            health = GetComponent<Health>();
+        }
+        private void Update()
+        {
+            /*
+             * Here we add priority to the intereact with combat boolean method
+             * If InteractWithcombat returns true than we return and boot out of the update
+             * else we continue onto the movement method.
+             */
+            if (health.IsDead()) return;
+            if (InteractWithCombat()) return;
+            if (InteractWithMovement()) return;
+        }
+
+        private bool InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+                if (!GetComponent<Fighter>().CanAttack(target.gameObject)) continue;
+                 
+                if(Input.GetMouseButton(0))
+                {
+                    GetComponent<Fighter>().Attack(target.gameObject);
+                }
+                return true;
+            }
+            //This returns false if target was never detected in the hits array.
+            return false;
+        }
+
+        private bool InteractWithMovement()
+        {
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if(hasHit)
+            {
+                if(Input.GetMouseButton(0) && !health.IsDead())
+                {
+                    GetComponent<Mover>().StartMoveAction(hit.point);
+                }
+                return true;
+            }
+            Debug.LogWarning("Area is not traversable");
+            return false;
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+}
